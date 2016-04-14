c2c8a4a3490a59cf83399c232396db58fed253f2
Tom LeSavage <your.email@example.com>
2016-04-13 21:36:51 -0700

    Started number question

--- app.js
+++ app.js
@@ -1,65 +1,46 @@
 var pElOne = document.getElementById("p1");
 var pElTwo = document.getElementById("p2");
 var pElThree = document.getElementById("p3");
+var pElFour = document.getElementById("p4");
 var counter = 0;
 
 //var userName = prompt("Hey, what's your name?");
 
-var els = [pElOne, pElTwo, pElThree];
+var els = [pElOne, pElTwo, pElThree, pElFour];
 var questions = [
  "Do you think my name is Nurbek or Sally?",
  "Do you think I am from here or Texas?",
- "Is tennis my favorite hobby?"
+ "Is tennis my favorite hobby?",
+ "How old is my son?"
 ];
 var answers = [
  "nurbek",
  "here",
- "yes"
+ "yes",
+ '6'
 ];
-
-// var name =  "nurbek"
-// var age = "34";
-// var textAge = "thirty four"
-// var city = "seattle";
 
 function game(question, answer, element){
   var answerName = prompt(question).toLowerCase();
   if (answerName === answer) {
     element.textContent = 'Great, you guessed it right';
   counter++
+      // else if (parseInt(answerName === '6')) {
+      // element.textContent = 'Indeed.'
   }  else {
     element.textContent = 'Sorry, you guessed wrong';
   }
 }
 
-// var answerAge = prompt ('What is my age?');
-// if (age === answerAge || answerAge.toLowerCase() === textAge){
-//   alert ('Great, you guessed it right');
-//   console.log('His or her\'s age match to this kind of work');
-// } else {
-//   alert ('Sorry, you guessed wrong');
-//   console.log('His or her\'s age DOES NOT match to this kind of work');
-// }
-//
-// var answerCity = prompt ('What is my city?')
-// if (answerCity.toLowerCase() === city) {
-//   alert ('Great, you guessed it right');
-//   console.log('User passed the test');
-// } else {
-//   alert ('Sorry, you guessed wrong')
-//   console.log ('User failed the test');
-// }
-
-// function game(question, answer, element) {
-//   var userInput = prompt(question);
-//   if (userInput === answer) {
-//     pElOne.innerHTML = questions[0];
-//     // Target element and update textContent;
-//   } else {
-//     // Do some other codes;
-//     // Target element and update textContent;
-//   }
-// }
 for (i = 0; i < questions.length; i++){  //WHICH ARRAY GOES HERE???
   game(questions[i], answers[i], els[i]);
 }
+// var guessAge = parseInt(prompt ("How old do you think my son is?"));
+// console.log("user input for guessAge " + guessAge);
+// if (guessAge === 3){
+//  alert("That's right! You guessed it.");
+//  counter++;
+// } else if (guessAge > 3) {
+//  alert("Too high. He's 3");
+// } else {alert("Close, he's 3");
+// }
