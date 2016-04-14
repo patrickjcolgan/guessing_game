8a75bf5888a1b97f5586e51f82aa9be892ff8ef7
Tom LeSavage <your.email@example.com>
2016-04-13 21:12:08 -0700

    Function syntax complete

--- app.js
+++ app.js
@@ -8,57 +8,58 @@ var counter = 0;
 var els = [pElOne, pElTwo, pElThree];
 var questions = [
  "Do you think my name is Nurbek or Sally?",
- "Do you think I am from Kyrgyzstan or Texas?",
+ "Do you think I am from here or Texas?",
  "Is tennis my favorite hobby?"
 ];
 var answers = [
- "Nurbek",
- "Kyrgyzstan",
- "Tennis"
+ "nurbek",
+ "here",
+ "yes"
 ];
 
-var name =  "nurbek"
-var age = "34";
-var textAge = "thirty four"
-var city = "seattle";
+// var name =  "nurbek"
+// var age = "34";
+// var textAge = "thirty four"
+// var city = "seattle";
 
-var answerName = prompt ('What is my name?');
-if (answerName.toLowerCase() === name) {
-alert ('Great, you guessed it right');
-  console.log ('User passed the test');
-}  else {
-  alert ('Sorry, you guessed wrong');
-console.log ('User failed the test');
-}
-
-var answerAge = prompt ('What is my age?');
-if (age === answerAge || answerAge.toLowerCase() === textAge){
-  alert ('Great, you guessed it right');
-  console.log('His or her\'s age match to this kind of work');
-} else {
-  alert ('Sorry, you guessed wrong');
-  console.log('His or her\'s age DOES NOT match to this kind of work');
-}
-
-var answerCity = prompt ('What is my city?')
-if (answerCity.toLowerCase() === city) {
-  alert ('Great, you guessed it right');
-  console.log('User passed the test');
-} else {
-  alert ('Sorry, you guessed wrong')
-  console.log ('User failed the test');
-}
-
-function game(question, answer, element) {
-  var userInput = prompt(question);
-  if (userInput === answer) {
-    pElOne.innerHTML = questions[0];
-    // Target element and update textContent;
-  } else {
-    // Do some other codes;
-    // Target element and update textContent;
+function game(question, answer, element){
+  var answerName = prompt(question).toLowerCase();
+  if (answerName === answer) {
+    element.textContent = 'Great, you guessed it right';
+  counter++
+  }  else {
+    element.textContent = 'Sorry, you guessed wrong';
   }
 }
-for (i = 0; i < question.length; i++){  //WHICH ARRAY GOES HERE???
-  game(question[i], answer[i], element[i]);
+
+// var answerAge = prompt ('What is my age?');
+// if (age === answerAge || answerAge.toLowerCase() === textAge){
+//   alert ('Great, you guessed it right');
+//   console.log('His or her\'s age match to this kind of work');
+// } else {
+//   alert ('Sorry, you guessed wrong');
+//   console.log('His or her\'s age DOES NOT match to this kind of work');
+// }
+//
+// var answerCity = prompt ('What is my city?')
+// if (answerCity.toLowerCase() === city) {
+//   alert ('Great, you guessed it right');
+//   console.log('User passed the test');
+// } else {
+//   alert ('Sorry, you guessed wrong')
+//   console.log ('User failed the test');
+// }
+
+// function game(question, answer, element) {
+//   var userInput = prompt(question);
+//   if (userInput === answer) {
+//     pElOne.innerHTML = questions[0];
+//     // Target element and update textContent;
+//   } else {
+//     // Do some other codes;
+//     // Target element and update textContent;
+//   }
+// }
+for (i = 0; i < questions.length; i++){  //WHICH ARRAY GOES HERE???
+  game(questions[i], answers[i], els[i]);
 }
