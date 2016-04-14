f2a70eeadb71b13b4a41ad362a05712386d0bf25
Patrick Colgan <patrickjcolgan@hotmail.com>
2016-04-13 19:27:37 -0700

    First Commit

--- app.js
+++ app.js
@@ -1,35 +1,54 @@
-var name =  "nurbek"
-var age = "34";
-var textAge = "thirty four"
-var city = "seattle";
+// var name =  "nurbek"
+// var age = "34";
+// var textAge = "thirty four"
+// var city = "seattle";
+//
+//
+//
+//
+// var answerName = prompt ('What is my name?');
+// if (answerName.toLowerCase() === name) {
+// alert ('Great, you guessed it right');
+//   console.log ('User passed the test');
+// }  else {
+//   alert ('Sorry, you guessed wrong');
+// console.log ('User failed the test');
+// }
+//
+// var answerAge = prompt ('What is my age?');
+// if (age === answerAge || answerAge.toLowerCase() === textAge){
+//   alert ('Great, you guessed it right');
+//   console.log('His or her\'s age match to this kind of work');
+// } else {
+//   alert ('Sorry, you guessed wrong');
+//   console.log('His or her\'s age DOES NOT match to this kind of work');
+// }
+//
+// var answerCity = prompt ('What is my city?')
+// if (answerCity.toLowerCase() === city) {
+//   alert ('Great, you guessed it right');
+//   console.log('User passed the test');
+// } else {
+//   alert ('Sorry, you guessed wrong')
+//   console.log ('User failed the test');
+//
+// }
+var pElOne = document.getElementById("p1");
+var pElTwo = document.getElementById("p2");
+var pElThree = document.getElementById("p3");
+var counter = 0;
 
+var userName = prompt("Hey, what's your name?");
+ //alert?????
 
-
-
-var answerName = prompt ('What is my name?');
-if (answerName.toLowerCase() === name) {
-alert ('Great, you guessed it right');
-  console.log ('User passed the test');
-}  else {
-  alert ('Sorry, you guessed wrong');
-console.log ('User failed the test');
-}
-
-var answerAge = prompt ('What is my age?');
-if (age === answerAge || answerAge.toLowerCase() === textAge){
-  alert ('Great, you guessed it right');
-  console.log('His or her\'s age match to this kind of work');
-} else {
-  alert ('Sorry, you guessed wrong');
-  console.log('His or her\'s age DOES NOT match to this kind of work');
-}
-
-var answerCity = prompt ('What is my city?')
-if (answerCity.toLowerCase() === city) {
-  alert ('Great, you guessed it right');
-  console.log('User passed the test');
-} else {
-  alert ('Sorry, you guessed wrong')
-  console.log ('User failed the test');
-
-}
+var els = [pElOne, pElTwo, pElThree];
+var questions [
+ "Do you think my name is Nurbek or Sally?",
+ "Do you think I am from Kyrgyzstan or Texas?",
+ "Is tennis my favorite hobby?"
+];
+var answers [
+ "Nurbek",
+ "Kyrgyzstan",
+ "Tennis"
+];
