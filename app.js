be7cd32575576432970898172422ecf7250f8829
Tom LeSavage <your.email@example.com>
2016-04-14 21:16:00 -0700

    added first parseInt + isNaN

--- app.js
+++ app.js
@@ -2,26 +2,34 @@ var pElOne = document.getElementById("p1");
 var pElTwo = document.getElementById("p2");
 var pElThree = document.getElementById("p3");
 var pElFour = document.getElementById("p4");
+var pElFive = document.getElementById("p5");
 var counter = 0;
 
 //var userName = prompt("Hey, what's your name?");
 
-var els = [pElOne, pElTwo, pElThree, pElFour];
+var els = [pElOne, pElTwo, pElThree, pElFour, pElFive];
 var questions = [
  "Do you think my name is Nurbek or Sally?",
  "Do you think I am from here or Texas?",
  "Is tennis my favorite hobby?",
  "How old is my son?"
+ "Guess a number between 1-10"
 ];
 var answers = [
  "nurbek",
  "here",
  "yes",
  '6'
+ 4
 ];
+
 
 function game(question, answer, element){
   var answerName = prompt(question).toLowerCase();
+  if (isNaN(parseInt(answerName)) === false) {
+    answerName = parseInt(answerName);
+  }
+
   if (answerName === answer) {
     element.textContent = 'Great, you guessed it right';
   counter++
@@ -35,12 +43,3 @@ function game(question, answer, element){
 for (i = 0; i < questions.length; i++){  //WHICH ARRAY GOES HERE???
   game(questions[i], answers[i], els[i]);
 }
-// var guessAge = parseInt(prompt ("How old do you think my son is?"));
-// console.log("user input for guessAge " + guessAge);
-// if (guessAge === 3){
-//  alert("That's right! You guessed it.");
-//  counter++;
-// } else if (guessAge > 3) {
-//  alert("Too high. He's 3");
-// } else {alert("Close, he's 3");
-// }
