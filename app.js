4fbfabbe90922aaef42beb2c4b9bcf6a46037dcb
Nurbek Ismailov <nurbekismailov@tests-MacBook-Air.local>
2016-04-16 14:59:24 -0700

    number answer working

--- app.js
+++ app.js
@@ -2,43 +2,58 @@ var pElOne = document.getElementById("p1");
 var pElTwo = document.getElementById("p2");
 var pElThree = document.getElementById("p3");
 var pElFour = document.getElementById("p4");
-var pElFive = document.getElementById("p5");
 var counter = 0;
 
 var userName = prompt("Hey, what's your name?");
 
-var els = [pElOne, pElTwo, pElThree, pElFour, pElFive];
+var els = [pElOne, pElTwo, pElThree, pElFour];
+
 var questions = [
  "Do you think my name is Nurbek or Sally?",
  "Do you think I am from here or Texas?",
- "Is tennis my favorite hobby?",
- "How old is my son?",
- "Guess a number between 1-10"
+ "Is tennis my favorite hobby, Yes or No ?",
+  "Guess a number between 1-10"
 ];
+
 var answers = [
  "nurbek",
  "here",
  "yes",
- '6',
  4
 ];
 
-
 function game(question, answer, element){
-  var answerName = prompt(question).toLowerCase();
-  if (isNaN(parseInt(answerName)) === false) {
-    answerName = parseInt(answerName);
+  var userInput = prompt(question).toLowerCase();
+
+  if (isNaN(parseInt(userInput)) === false) {
+    userInput = parseInt(userInput);
   }
 
-  if (answerName === answer) {
-    element.textContent = 'Great, you guessed it right';
-  counter++
-      // else if (parseInt(answerName === '6')) {
-      // element.textContent = 'Indeed.'
-  }  else {
-    element.textContent = 'Sorry, you guessed wrong';
+  if (typeof answer === 'number') {
+    while (userInput !== answer) {
+      if (userInput < answer) {
+        element.textContent = 'your guess is too low';
+        userInput = parseInt(prompt('Guess again.'))
+      } else if (userInput > answer) {
+        element.textContent = 'your guess is too high';
+        userInput = parseInt(prompt('Guess again.'))
+      }
+    }
+    element.textContent = 'Great, you guessed the number right';}
   }
-}
+    // need to handle getting the correct number here
+
+  // else {
+  //   if (userInput === answer) { // input matches answer
+  //
+  //     element.textContent = 'Great, you guessed it right';
+  //     counter++
+  //   } else {
+  //     element.textContent = 'Sorry, you guessed wrong';
+  //   }
+  // }
+
+
 
 for (i = 0; i < questions.length; i++){  //WHICH ARRAY GOES HERE???
   game(questions[i], answers[i], els[i]);
